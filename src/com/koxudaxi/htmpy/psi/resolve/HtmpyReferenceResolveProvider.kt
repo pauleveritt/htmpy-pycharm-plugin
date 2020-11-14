f6b6a68a9a6a250400d257399593fb9c583519b7
Koudai Aono <koxudaxi@gmail.com>
2020-11-15 05:49:56 +0900

    fix unresolved comprehensions

--- src/com/koxudaxi/htmpy/psi/resolve/HtmpyReferenceResolveProvider.kt
+++ src/com/koxudaxi/htmpy/psi/resolve/HtmpyReferenceResolveProvider.kt
@@ -1,9 +1,11 @@
 package com.koxudaxi.htmpy.psi.resolve
 
-import com.jetbrains.python.codeInsight.dataflow.scope.ScopeUtil
+import com.intellij.psi.impl.source.resolve.FileContextUtil
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.refactoring.suggested.endOffset
+import com.intellij.refactoring.suggested.startOffset
 import com.jetbrains.python.documentation.doctest.PyDocstringLanguageDialect
-import com.jetbrains.python.psi.PyQualifiedExpression
-import com.jetbrains.python.psi.PyReferenceExpression
+import com.jetbrains.python.psi.*
 import com.jetbrains.python.psi.resolve.*
 import com.jetbrains.python.psi.types.TypeEvalContext
 
@@ -12,12 +14,16 @@ class HtmpyReferenceResolveProvider : PyReferenceResolveProvider {
     override fun resolveName(element: PyQualifiedExpression, context: TypeEvalContext): List<RatedResolveResult> {
         if (element.containingFile.language != PyDocstringLanguageDialect.getInstance()) return emptyList()
         if (element !is PyReferenceExpression) return emptyList()
-        val referencedName = element.referencedName ?: return emptyList()
-        val originalOwner = ScopeUtil.getScopeOwner(element)
 
-        return if (originalOwner != null) {
-            PyResolveUtil.resolveLocally(originalOwner, referencedName)
-                    .map { RatedResolveResult(RatedResolveResult.RATE_NORMAL, it) }
-        } else emptyList()
+        // For comprehension
+        val psiRange = element.containingFile.getUserData(FileContextUtil.INJECTED_IN_ELEMENT)?.psiRange
+                ?: return emptyList()
+        return PyResolveUtil.resolveLocally(element).filter {
+            PsiTreeUtil.getTopmostParentOfType(it, PyComprehensionElement::class.java)?.let { listCompExpression ->
+                listCompExpression.startOffset < psiRange.startOffset && listCompExpression.endOffset > psiRange.endOffset
+            } ?: false
+        }.map {
+            RatedResolveResult(RatedResolveResult.RATE_NORMAL, it)
+        }
     }
 }
